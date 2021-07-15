92d94d549a5694a77bb3e0d634e1d38d2aaf4d12
Wojtek <wojlot@mail.com>
2021-07-15 08:43:33 +0100

    defi

--- 4. Feature Eng/02. Data Enhancement/data_enhancement.py
+++ 4. Feature Eng/02. Data Enhancement/data_enhancement.py
@@ -1,5 +1,4 @@
 
-
 import numpy    as np
 from numpy.testing._private.utils import decorate_methods
 import pandas   as pd
@@ -25,7 +24,7 @@ from xgboost               import XGBRegressor
 from lightgbm              import LGBMRegressor
 from catboost              import CatBoostRegressor
 
-data = pd.read_csv(r'data\london_merged.csv')
+data = pd.read_csv(r'Data\london_merged.csv')
 
 np.random.seed(0)
 
